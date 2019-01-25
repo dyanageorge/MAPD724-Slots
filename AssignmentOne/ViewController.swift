19e7bfcf72c2ef5179b65ac051125662ba23a58e
Dyana George <dyanageorge@Dyanas-MacBook-Air.local>
2019-01-24 19:40:38 -0500

    buttons added

--- AssignmentOne/ViewController.swift
+++ AssignmentOne/ViewController.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    @IBOutlet weak var bodyImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bodyImage.layer.cornerRadius = 20
-        bodyImage.clipsToBounds = true
-//        bodyImage.layer.borderColor = UIColor.gray.cgColor
-//        bodyImage.layer.borderWidth = 20
     }
     
 
